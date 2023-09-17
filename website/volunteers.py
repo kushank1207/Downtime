995d769dfe0302d0bfa40f581abcb26028458df2
chisa-f <kiki4537fer2cat@gmail.com>
2023-09-17 10:04:15 -0400

    add ids in modal

--- website/volunteers.py
+++ website/volunteers.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from datetime import datetime
 from . import db
-from .models import Event, User
+from .models import Event, User, Prize
 from sqlalchemy import select
 from flask_login import login_required, current_user
 
@@ -13,6 +13,7 @@ volunteers = Blueprint("volunteers", __name__)
 @login_required
 def home():
     events = Event.query.all()
+    prizes = Prize.query.filter(Prize.quantity > 0)
     previous_attended_events, future_events = [], []
     for event in events:
         event_date = datetime.strptime(event.start_date, '%Y-%m-%dT%H:%M')
@@ -21,5 +22,7 @@ def home():
         else:
             future_events.append(event)
         event.start_date = event_date.strftime("%b %d - %H:%M EST")
-    return render_template("volunteers.html", user=current_user, previous_attended_events=previous_attended_events, future_events=future_events)
+    return render_template("volunteers.html", user=current_user, previous_attended_events=previous_attended_events, future_events=future_events, available_items=prizes)
 
+# @volunteers.route("/purchase-item")
+# def purchase_item():
