ff72432e24e19d491f17b7383cbac165ac4bd736
Oz0neX <kingyasir8327@gmail.com>
2023-09-17 07:30:02 -0700

    Description update

--- website/static/js/form.js
+++ website/static/js/form.js
@@ -19,4 +19,18 @@ function displayDashboard(dashId) {
     document.getElementById(dashId).style = "display: block;";
 }
 
+function transferDataToEventModal() {
+
+}
+
+function transferDataToItemModal(element, x, y) {
+    displayForm(x, y);
+    for (const child of element.children) {
+        console.log(child);
+        if (child.className == "description") {
+            document.getElementById("description-m").innerText = child.innerText;
+        }
+    }
+}
+
 console.log("Javascript loaded! LOLZ")
